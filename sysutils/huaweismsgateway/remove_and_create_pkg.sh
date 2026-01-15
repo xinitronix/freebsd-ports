df2052e1f7558dce6177836e3f2ebbd1f6885bcd
Your Name <you@example.com>
2026-01-15 11:57:01 +0300

    изменено:      sysutils/huaweismsgateway/Makefile 	изменено:      sysutils/huaweismsgateway/distinfo 	новый файл:    sysutils/huaweismsgateway/rebuild.sh 	новый файл:    sysutils/huaweismsgateway/remove_and_create_pkg.sh

---
+++ sysutils/huaweismsgateway/remove_and_create_pkg.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+PKG=$(ls $HOME/2TB/All15 | grep huaweismsgateway )
+
+doas rm $HOME/2TB/All15/$PKG
+sshpass -p 639639 ssh pi@192.168.8.45 "rm ~/All15/$PKG"
+cd $HOME/2TB/All15/
+doas pkg create huaweismsgateway
+cd -
+doas pkg repo /ntfs-2TB/All15
+
+ncftpput  -R -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/      /ntfs-2TB/All15
+ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All15   /ntfs-2TB/All15/packagesite.txz
+ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All15   /ntfs-2TB/All15/meta.txz
+ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All15        /ntfs-2TB/All15/packagesite.txz
+sshpass -p 639639 ssh pi@192.168.8.45 'chmod -R 777 /home/pi/All15'
+
