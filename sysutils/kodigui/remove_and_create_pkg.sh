851d45f3a0a83030e45651a27578ce09e2f96e2f
Your Name <you@example.com>
2026-09-04 06:37:42 +0300

    изменено:      x11-wm/mjwm (изменено содержимое, неотслеживаемое содержимое)

--- sysutils/kodigui/remove_and_create_pkg.sh
+++ sysutils/kodigui/remove_and_create_pkg.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
+REPO=All15
+PKG=$(ls $HOME/2TB/$REPO | grep KodiGui)
 
-PKG=$(ls $HOME/2TB/All16 | grep KodiGui)
-
-doas rm $HOME/2TB/All16/$PKG
-sshpass -p 639639 ssh pi@192.168.8.45 "rm ~/All16/$PKG"
-cd $HOME/2TB/All16/
+doas rm $HOME/2TB/$REPO/$PKG
+sshpass -p 639639 ssh pi@192.168.8.45 "rm ~/$REPO/$PKG"
+cd $HOME/2TB/$REPO/
 doas pkg create KodiGui
 cd -
-doas pkg repo /ntfs-2TB/All16
+doas pkg repo /ntfs-2TB/$REPO
 
-ncftpput  -R -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/      /ntfs-2TB/All16
-ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All15   /ntfs-2TB/All16/packagesite.txz
-ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All15   /ntfs-2TB/All16/meta.txz
-ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All15        /ntfs-2TB/All16/packagesite.txz
-sshpass -p 639639 ssh pi@192.168.8.45 'chmod -R 777 /home/pi/All16'
+ncftpput  -R -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/      /ntfs-2TB/$REPO
+ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/$REPO   /ntfs-2TB/$REPO/packagesite.txz
+ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/$REPO   /ntfs-2TB/$REPO/meta.txz
+ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/$REPO        /ntfs-2TB/$REPO/packagesite.txz
+sshpass -p 639639 ssh pi@192.168.8.45 'chmod -R 777 /home/pi/$REPO'
 
